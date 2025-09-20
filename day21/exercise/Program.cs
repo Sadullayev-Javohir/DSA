c81cbd6b0fc2eddcb5fbfc6be122de9302d678f0
Sadullayev-Javohir <javohirsadullayev2024@gmail.com>
2025-09-20 19:42:57 +0500

    day21

---
+++ day21/exercise/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+  public static void HeapSort(int[] arr)
+  {
+    int n = arr.Length;
+      
+    for (int i = n / 2 - 1; i >= 0; i--)
+      Heapify(arr, n, i);
+
+    for (int end = n - 1; end > 0; end--)
+    {
+      Swap(arr, 0, end);
+      Heapify(arr, end, 0);
+    }
+  }
+
+  private static void Heapify(int[] arr, int heapSize, int i)
+  {
+    while (true)
+    {
+      int largest = i;
+      int left = 2 * i + 1;
+      int right = 2 * i + 2;
+
+      if (left < heapSize && arr[left] > arr[largest])
+        largest = left;
+      if (right < heapSize && arr[right] > arr[largest])
+        largest = right;
+      if (largest == i) break;
+
+      Swap(arr, i, largest);
+      i = largest;
+    }
+  }
+
+
+}
