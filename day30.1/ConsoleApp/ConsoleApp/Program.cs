acfe69b8413a8261d7589cf8fe605227a9e6779b
Javohir <javohirsadullayev2024@gmail.com>
2026-03-03 18:03:23 +0500

    day30.1

---
+++ day30.1/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 8, 3, 5, 9, 1 };
+
+        QuickSort(arr, 0, arr.Length - 1);
+        Console.WriteLine(string.Join(", ", arr));
+    }
+
+    static void QuickSort(int[] arr, int left, int right)
+    {
+        if (left >= right) return;
+
+        int pivotIndex = Random.Shared.Next(left, right + 1);
+        int pivot = arr[pivotIndex];
+
+        int index = Partition(arr, left, right, pivot);
+
+        QuickSort(arr, left, index - 1);
+        QuickSort(arr, index, right);
+    }
+
+    static int Partition(int[] arr, int left, int right, int pivot)
+    {
+        while (left <= right)
+        {
+            while (arr[left] < pivot) left++;
+            while (arr[right] > pivot) right--;
+
+            if (left <= right)
+            {
+                (arr[left], arr[right]) = (arr[right], arr[left]);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
+}
